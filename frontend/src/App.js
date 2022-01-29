841cca08aec4777e3ad8cb6f866a32e68a9f390b
Kristina Kurshakova <kurshakovakristina@gmail.com>
2022-01-29 23:08:51 +0300

    Refactor Navigation.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,10 +3,10 @@ import './css/App.css';
 import './css/Bootsrtap-template.css';
 import DataProvider from './DataProvider';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Button from 'react-bootstrap/Button';
 import LeftNavigation from './components/LeftNavigation';
-import PortfolioTable from './components/PortfolioTable';
-import StocksRubPage from './pages/Stocks.Rub.Page';
+import PortfolioPage from './pages/portfolio/Portfolio.Page';
+import AssetPage from './pages/asset/Asset.Page';
+
 import {
   BrowserRouter as Router,
   Switch,
@@ -14,22 +14,21 @@ import {
   Link
 } from "react-router-dom";
 
-
-
 function App() {
   const [portfolio, setPortfolio] = useState(null);
   const [accounts, setAccounts] = useState(null);
   const [currenciesBalance, setCurrenciesBalance] = useState(null);
+  const [allStocks, setAllStocks] = useState(null);
 
-   async function loadData(){
+  async function loadData(){
     const portfolio = await DataProvider.fetchPortfoloio();
     const accounts = await DataProvider.fetchAccount();
     const currenciesBalance = await DataProvider.fetchCurrenciesBalance();
+    const allStocks = await DataProvider.fetchAllStocks();
     setCurrenciesBalance(currenciesBalance);
     setPortfolio(portfolio);
     setAccounts(accounts);
-
-    console.log(accounts, currenciesBalance);
+    setAllStocks(allStocks);
   }
 
   useEffect(() => {
@@ -42,29 +41,34 @@ function App() {
   return (
     <div className="App">
       <nav class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
-        <a class="navbar-brand col-md-3 col-lg-2 mr-0 px-3" href="#">Мои Тинькофф Инвестиции</a>
+        <a class="navbar-brand col-md-3 col-lg-2 mr-0 px-3 app-title" href="#">Мои Тинькофф Инвестиции</a>
       </nav>
 
-      <div class="container-fluid">
-        <div class="row">
+      <div class="container-fluid app-content-wrapper">
           <Router>
             <LeftNavigation />
-            <main role="main" class="col-md-9 ml-sm-auto col-lg-10 px-md-4">
+            <main role="main" class="app-content col-md-9 ml-sm-auto col-lg-10 px-md-4">
             <Switch>
               <Route path="/stocks/rus">
-                <StocksRubPage data={portfolio.stocks.rus} currencyBalance={currenciesBalance.RUB} currency={"RUB"} />
+                <PortfolioPage data={portfolio.stocks.rus} currencyBalance={currenciesBalance.RUB} currency={"RUB"} />
               </Route>
               <Route path="/stocks/usd">
-                <StocksRubPage data={portfolio.stocks.usd} currencyBalance={currenciesBalance.USD} currency={"USD"} />
+                <PortfolioPage data={portfolio.stocks.usd} currencyBalance={currenciesBalance.USD} currency={"USD"} />
               </Route>
               <Route path="/stocks/eur">
-                <StocksRubPage data={portfolio.stocks.eur} currencyBalance={currenciesBalance.EUR} currency={"EUR"} />
+                <PortfolioPage data={portfolio.stocks.eur} currencyBalance={currenciesBalance.EUR} currency={"EUR"} />
               </Route>
+              <Route path="/etfs">
+                <PortfolioPage data={portfolio.etfs} currencyBalance={currenciesBalance.RUB} currency={"EUR"} />
+              </Route>
+              <Route path="/bonds">
+                <PortfolioPage data={portfolio.bonds} currencyBalance={currenciesBalance.RUB} currency={"EUR"} />
+              </Route>
+              <Route path="/asset/:ticker" component={AssetPage} />
             </Switch>
               
             </main>
           </Router>
-        </div>
       </div>
     
     </div>
